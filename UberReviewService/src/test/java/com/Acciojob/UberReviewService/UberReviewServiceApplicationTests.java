bb715b2c559b35465d1429d9f94ef8850d06322c
vikash-mdh <vikashkumar555550@gmail.com>
2025-06-15 11:35:33 +0530

    first commit

---
+++ UberReviewService/src/test/java/com/Acciojob/UberReviewService/UberReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Acciojob.UberReviewService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
